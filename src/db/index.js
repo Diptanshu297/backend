06cf00bc33208c0303ecd062ff8649e53be49561
Diptanshu297 <dipdatta1973@gmail.com>
2025-06-18 21:03:51 +0530

    add structure changes

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,17 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from '../constants.js';
+// src/db/index.js
 
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
+// Connect to MongoDB
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}')
-        console.log('\n Mongodb connected !! DB HOST: ${connectionInstance.connection.host}');
-        
-    } catch (error) {
-        console.log("MONGODB connection error",error);
-        process.exit(1);
-    }
-}
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n✅ MongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1); // Exit the app on DB connection failure
+  }
+};
 
-export default connectDB
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import dotenv from "dotenv"
-import mongoose from 'mongoose';
-import { DB_NAME} from './constants';
+
 import connectDB from './db/index.js';
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 connectDB()
