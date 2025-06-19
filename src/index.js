83366b47fd4c52e3ec12b9b2d78a08e58e4455af
Diptanshu297 <dipdatta1973@gmail.com>
2025-06-19 13:25:08 +0530

    add structures

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import dotenv from "dotenv"
-
-import connectDB from './db/index.js';
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js"; 
 
 dotenv.config({
-    path:'./.env'
-})
+  path: "./.env"
+});
 
 connectDB()
-.then(()=> {
-     app.on("error", (error) =>{
-        console.log("error", error);
-        throw error
-       })
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Server error:", error);
+      throw error;
+    });
 
-
-  app.listen(process.env.PORT  || 8000, () =>{
-     console.log(`Server is running at port: ${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`✅ Server is running at port: ${process.env.PORT}`);
+    });
   })
-})
-.catch((err) => {
-     console.log("MONGO DB connection fail !!!", err);
-})
+  .catch((err) => {
+    console.log("❌ MONGO DB connection fail !!!", err);
+  });
+
 
 /*
 import express from 'express';
